ca8e54292811184d0c8fcff4757d2452c0b7b35f
Manpreetsin <sing8711@sheridancollege.ca>
2019-06-18 19:13:35 -0400

    Merge origin/RABIA_FARHANA_ICE1 into Practice
    Conflicts:
    	build.xml
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml

---
+++ src/practices/App.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practices;
+
+/**
+ *
+ * @author Manpreet Singh
+ */
+public class App {
+	private int name;
+	private int color;
+
+	public void getName() {
+		// TODO - implement Application.getName
+		throw new UnsupportedOperationException();
+		//ManpreetSingh
+                //age18
+		//good boy
+	}
+
+	
+	public void setName(int name) {
+		this.name = name;
+	}
+
+	public void getColor() {
+		// TODO - implement Application.getColor
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param color
+	 */
+	public void setColor(int color) {
+		this.color = color;
+	}
+    
+}
+
