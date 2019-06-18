d441e602ebfae5dbf74b1e8e5d2b9de8479252f9
Manpreetsin <sing8711@sheridancollege.ca>
2019-06-18 18:30:29 -0400

    Student Demo Program

---
+++ src/project/Application1.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project;
+
+/**
+ *
+ * @author Manpreet Singh
+ */
+public class Application1 {
+
+	private int Player;
+	private int PlayerName;
+
+	public void getPlayer() {
+		// TODO - implement Application1.getPlayer
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Player
+	 */
+	public void setPlayer(int Player) {
+		// TODO - implement Application1.setPlayer
+		throw new UnsupportedOperationException();
+	}
+
+	public void getPlayerName() {
+		// TODO - implement Application1.getPlayerName
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param PlayerName
+	 */
+	public void setPlayerName(int PlayerName) {
+		// TODO - implement Application1.setPlayerName
+		throw new UnsupportedOperationException();
+	}
+    
+}
